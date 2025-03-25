ec7fcbb6805179b269b737e2a42823a3cf4187ea
Bruno Fuentes <brunofuentess@gmail.com>
2025-03-25 20:52:37 -0300

    change step name

--- src/database/enums.py
+++ src/database/enums.py
@@ -2,7 +2,7 @@ import enum
 
 
 class StepName(enum.Enum):
-    EXTRACT_CONTENT = "extract_content"
+    SCRAPING = "scraping"
     SYNTAX_CHECK = "syntax_check"
     QUALITY_CHECK = "quality_check"
     REPORT_GEN = "report_gen"
